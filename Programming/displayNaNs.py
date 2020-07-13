51df687f276b35132a88d762971ba0b9141d4130
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-07-13 20:21:47 +0100

    Update displayNaNs.py
    - Renaming file paths.
    - Adding a new comments on line 21 and line 22.

--- Programming/displayNaNs.py
+++ Programming/displayNaNs.py
@@ -8,8 +8,8 @@ pd.options.display.max_rows = 500
 
 # Load file; in this case a CSV file from Oxford Uni on DD/MM/YY
 # Syntax: df = pd.read_csv('file/path/file_name.csv')
-df = pd.read_csv('D:/_00 UN Volunteers/22 June - Ongoing/Oxford Uni Dataset/07-20/OxCGRT_09-7-20.csv')
-file_name = os.path.basename('D:/_00 UN Volunteers/22 June - Ongoing/Oxford Uni Dataset/09-20/OxCGRT_13-7-20.csv')
+df = pd.read_csv('file/path/file_name.csv')
+file_name = os.path.basename('file/path/file_name.csv')
 
 # Counting NaNs inside the DataFrame
 # Syntax: count=df.isna().sum()
@@ -18,5 +18,7 @@ count = df.isna().sum()
 print("The DataFrame is using " + file_name)
 print("NaN counts for this DataFrame is:\n" + str(count))
 
+# Display the visualization of missing values from the Data Frame
+# In this example, use 'bar graph' to plot it
 msno.bar(df.drop('CountryName', axis='columns').sample(500))
 plt.show()
