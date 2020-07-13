6141e01de69d81429fd82184f6518ff6a53277a6
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-07-13 20:19:14 +0100

    Update dataViz.py
    - Renaming file path.
    - Uncommented part of the commented Python script.

--- Programming/dataViz.py
+++ Programming/dataViz.py
@@ -2,11 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from plotly.graph_objs import Scatter, Layout
-
 # Load file; in this case a CSV file from Oxford Uni on DD/MM/YY
 # Syntax: df = pd.read_csv('file/path/file_name.csv')
-df = pd.read_csv('D:/_00 UN Volunteers/22 June - Ongoing/Oxford Uni Dataset/07-20/OxCGRT_11-7-20.csv')
+df = pd.read_csv('file/path/file_name.csv')
 
 # List of the column names from flat file (CSV file)
 # Begins with the countries name, its code and the date when information was collected
@@ -74,27 +72,27 @@ m_ConfDeaths = np.isfinite(ConfDeaths)  # NaN: 10412
 
 # Plot 1 - Country by Code and Confirmed Cases
 # Syntax: plt.plot_name(x_axis, y_axis)
-# plt.bar(CountryCode, ConfCases / 10**6)
-# plt.xlabel('CountryCode')
-# plt.ylabel('ConfirmedCases \n (in Millions)')
-# plt.title('COVID-19 Confirmed Cases \n 01-01-2020 to 09-07-20')
-# plt.xticks(rotation=90)  # Rotate the value of X-axis points into 90 degree
-# plt.tick_params(axis='x', which='major', labelsize=6.15)  # Increase space between points on X-axis
-# plt.rc('grid', linestyle="-", color='lightblue')
-# plt.grid(True)
-# plt.show()
+plt.bar(CountryCode, ConfCases / 10**6)
+plt.xlabel('CountryCode')
+plt.ylabel('ConfirmedCases \n (in Millions)')
+plt.title('COVID-19 Confirmed Cases \n 01-01-2020 to 09-07-20')
+plt.xticks(rotation=90)  # Rotate the value of X-axis points into 90 degree
+plt.tick_params(axis='x', which='major', labelsize=6.15)  # Increase space between points on X-axis
+plt.rc('grid', linestyle="-", color='lightblue')
+plt.grid(True)
+plt.show()
 
 # Plot 2 - Country by Code and Confirmed Deaths
 # Syntax: plt.plot_name(x_axis, y_axis)
-# plt.bar(CountryCode, ConfDeaths)
-# plt.xlabel('CountryCode')
-# plt.ylabel('ConfirmedDeaths \n (in Thousands)')
-# plt.title('COVID-19 Confirmed Deaths \n 01-01-2020 to 09-07-20')
-# plt.xticks(rotation=90)  # Rotate the value of X-axis points into 90 degree
-# plt.tick_params(axis='x', which='major', labelsize=6.15)  # Increase the size of points label on X-axis
-# plt.rc('grid', linestyle="-", color='lightblue')
-# plt.grid(True)
-# plt.show()
+plt.bar(CountryCode, ConfDeaths)
+plt.xlabel('CountryCode')
+plt.ylabel('ConfirmedDeaths \n (in Thousands)')
+plt.title('COVID-19 Confirmed Deaths \n 01-01-2020 to 09-07-20')
+plt.xticks(rotation=90)  # Rotate the value of X-axis points into 90 degree
+plt.tick_params(axis='x', which='major', labelsize=6.15)  # Increase the size of points label on X-axis
+plt.rc('grid', linestyle="-", color='lightblue')
+plt.grid(True)
+plt.show()
 
 # Plot 3 - Date and Confirmed Cases
 # Syntax: plt.plot_name(x_axis, y_axis)
@@ -112,24 +110,24 @@ plt.show()
 
 # Plot 4 - Date and Confirmed Deaths
 # Syntax: plt.plot_name(x_axis,y_axis)
-# plt.bar(f_DateC, ConfDeaths)
-# plt.xlabel('Dates \n (YYYY-MM)')
-# plt.ylabel('ConfirmedDeaths \n (in Thousands)')
-# plt.title('COVID-19 Confirmed Deaths \n 01-01-2020 to 09-07-20')
-# plt.xticks(rotation=45)  # Rotate the value of X-axis points into 45 degree
-# plt.tick_params(axis='x', which='major', labelsize=8)  # Increase the size of points label on X-axis
-# plt.rc('grid', linestyle="-", color="lightblue")
-# plt.grid(True)
-# plt.show()
+plt.bar(f_DateC, ConfDeaths)
+plt.xlabel('Dates \n (YYYY-MM)')
+plt.ylabel('ConfirmedDeaths \n (in Thousands)')
+plt.title('COVID-19 Confirmed Deaths \n 01-01-2020 to 09-07-20')
+plt.xticks(rotation=45)  # Rotate the value of X-axis points into 45 degree
+plt.tick_params(axis='x', which='major', labelsize=8)  # Increase the size of points label on X-axis
+plt.rc('grid', linestyle="-", color="lightblue")
+plt.grid(True)
+plt.show()
 
 # Plot 5 - Date and School Closing
 # Syntax: plt.plot_name(x_axis, y_axis)
-# plt.bar(f_DateC, c1_SchoolClosing)
-# plt.xlabel('Dates \n (YYYY-MM)')
-# plt.ylabel('Containment and Closure Policies \n [School Closing]')
-# plt.title('COVID-19 Governments Respond \n School Closing \n 01-01-2020 to 09-07-2020')
-# plt.xticks(rotation=45)  # Rotate the value of X-axis points into 45 degree
-# plt.tick_params(axis='x', which='major', labelsize=8)  # Increase the size of points label on X-axis
-# plt.rc('grid', linestyle="-", color="lightblue")
-# plt.grid(True)
-# plt.show()
+plt.bar(f_DateC, c1_SchoolClosing)
+plt.xlabel('Dates \n (YYYY-MM)')
+plt.ylabel('Containment and Closure Policies \n [School Closing]')
+plt.title('COVID-19 Governments Respond \n School Closing \n 01-01-2020 to 09-07-2020')
+plt.xticks(rotation=45)  # Rotate the value of X-axis points into 45 degree
+plt.tick_params(axis='x', which='major', labelsize=8)  # Increase the size of points label on X-axis
+plt.rc('grid', linestyle="-", color="lightblue")
+plt.grid(True)
+plt.show()
