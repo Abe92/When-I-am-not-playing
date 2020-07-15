74d7cb0d7763e5dc0eb36130028b93e8de6a9c15
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-07-15 18:14:12 +0100

    Update plotlyCombinations.py
    - Redacting the values
    - Refactor the comments

--- Programming/Python Scripts/plotlyCombinations.py
+++ Programming/Python Scripts/plotlyCombinations.py
@@ -4,10 +4,10 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 # Set up variable to use for connecting to Postgres Database
-PGHOST = 'localhost'
-PGDATABASE = 'UN_Volunteers'
-PGUSER = 'postgres'
-PGPASSWORD = 'admin123'
+PGHOST = 'your_host_name'
+PGDATABASE = 'your_database_name'
+PGUSER = 'your_username'
+PGPASSWORD = 'your_password'
 
 # Set up connection to the Postgres Server [local server + local database]
 conn_string = "host=" + PGHOST + " port=" + "5432" + " dbname=" + PGDATABASE + " user=" + PGUSER + " password=" \
@@ -16,16 +16,16 @@ conn = psycopg2.connect(conn_string)
 print("Connection is successfully established to " + PGDATABASE)
 
 # Testing SQL Query through Python
-UN_Data = pd.read_sql('SELECT * FROM "govt_respond_tracker" ORDER BY datec, countryname ASC', conn)
-print(UN_Data.head(10))  # Print the first 10 rows
+DataFrame = pd.read_sql('SELECT * FROM "table_name" ORDER BY column1, column2 ASC', conn)
+print(DataFrame.head(10))  # Print the first 10 rows
 
 # Building plot using data from Postgres Table
-fig_line = px.line(UN_Data,
-                   x='countryname',
-                   y='confirmedcases',
-                   color='countrycode')
-# fig_line.update_xaxes(showgrid=True,
-#                       ticks="outside",            # It looks horrendous with this alteration
-#                       tickson="boundaries",       # These lines function;
-#                       ticklen=20)                 # Placing ticks and gridlines between categories
+fig_line = px.line(DataFrame,
+                   x='column1',
+                   y='column2',
+                   color='column3')
+fig_line.update_xaxes(showgrid=True,
+                       ticks="outside",            # OPTIONAL
+                       tickson="boundaries",       
+                       ticklen=20)                 
 fig_line.show()
