bd07e0b82b22d5ae7cac0a759de9b0bf10d704a0
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-07-24 23:55:40 +0100

    Update AfricanQuery.sql
    - Reformat indentation for clean preview

--- Programming/SQL Scripts/AfricanQuery.sql
+++ Programming/SQL Scripts/AfricanQuery.sql
@@ -4,15 +4,15 @@ SQL Query to call all available African countries from 'govt_respond_tracker' ta
 
 SELECT * FROM govt_respond_tracker
 WHERE countryname IN ('Algeria', 'Angola', 'Benin', 'Bostwana',
-					 'Burkina Faso', 'Burundi', 'Cameroon', 'Cape Verde',
-					 'Central African Republic', 'Chad', 'Congo', 'Cote d''Ivoire',
-					 'Democratic Republic of the Congo', 'Djibouti', 'Egypt',
-					 'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia',
-					 'Ghana', 'Guinea', 'Kenya', 'Lesotho', 'Liberia', 'Libya',
-					 'Madagascar', 'Malawi', 'Mauritania', 'Mauritius', 'Morocco',
-					 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda', 'Seychelles',
-					 'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan',
-					 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia')
+		      'Burkina Faso', 'Burundi', 'Cameroon', 'Cape Verde',
+	              'Central African Republic', 'Chad', 'Congo', 'Cote d''Ivoire',
+		      'Democratic Republic of the Congo', 'Djibouti', 'Egypt',
+		      'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia',
+		      'Ghana', 'Guinea', 'Kenya', 'Lesotho', 'Liberia', 'Libya',
+	              'Madagascar', 'Malawi', 'Mauritania', 'Mauritius', 'Morocco',
+		      'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda', 'Seychelles',
+		      'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan',
+		      'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia')
 ORDER BY 
 countryname ASC,
 datec ASC;
