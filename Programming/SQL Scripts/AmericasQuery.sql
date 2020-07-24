15eff61a8f58ec4881e85b113b6865bf394572df
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-07-24 23:54:32 +0100

    Update AmericasQuery.sql
    - Reformat indentation for clean preview

--- Programming/SQL Scripts/AmericasQuery.sql
+++ Programming/SQL Scripts/AmericasQuery.sql
@@ -4,11 +4,11 @@ SQL Query to call all available Americas countries from 'govt_respond_tracker' t
 
 SELECT * FROM govt_respond_tracker
 WHERE countryname IN ('Argentina', 'Barbados', 'Belize', 'Bolivia', 'Brazil',
-					 'Canada', 'Chile', 'Colombia', 'Costa Rica', 'Cuba', 'Dominica',
-					 'Dominican Republic', 'Ecuador', 'El Salvador', 'Guatemala', 'Guyana',
-					 'Haiti', 'Honduras', 'Jamaica', 'Mexico', 'Nicaragua', 'Panama',
-					 'Paraguay', 'Peru', 'Suriname', 'Trinidad and Tobago', 'United States',
-					 'Uruguay', 'Venezuela')
+		      'Canada', 'Chile', 'Colombia', 'Costa Rica', 'Cuba', 'Dominica',
+		      'Dominican Republic', 'Ecuador', 'El Salvador', 'Guatemala', 'Guyana',
+		      'Haiti', 'Honduras', 'Jamaica', 'Mexico', 'Nicaragua', 'Panama',
+		      'Paraguay', 'Peru', 'Suriname', 'Trinidad and Tobago', 'United States',
+		      'Uruguay', 'Venezuela')
 ORDER BY 
 countryname ASC,
 datec ASC;
