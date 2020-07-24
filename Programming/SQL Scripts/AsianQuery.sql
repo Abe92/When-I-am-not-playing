5a373917a38f8b2b9e5a768b5308d32c565ff836
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-07-24 23:53:20 +0100

    Update AsianQuery.sql
    - Reformat indentation for clean preview

--- Programming/SQL Scripts/AsianQuery.sql
+++ Programming/SQL Scripts/AsianQuery.sql
@@ -4,14 +4,14 @@ SQL Query to call all available Asian countries from 'govt_respond_tracker' tabl
 
 SELECT * FROM govt_respond_tracker
 WHERE countryname IN ('Afghanistan', 'Bahrain', 'Bangladesh', 'Bhutan', 'Brunei',
-					 'Cambodia', 'China', 'Hong Kong', 'India', 'Indonesia', 'Iran',
-					 'Iraq', 'Japan', 'Jordan', 'Kuwait', 'Kyrgyz Republic', 'Laos',
-					 'Lebanon', 'Macao', 'Malaysia', 'Mongolia', 'Myanmar', 'Nepal',
-					 'Oman', 'Pakistan', 'Philippines', 'Qatar', 'Saudi Arabia',
-					 'Singapore', 'South Korea', 'Sri Lanka', 'Palestine', 'Syria',
-					 'Taiwan', 'Tajikistan', 'Thailand', 'Timor-Leste', 'Turkey', 
-					 'Turkmenistan', 'United Arab Emirates', 'Uzbekistan', 'Vietnam',
-					 'Yemen')
+		      'Cambodia', 'China', 'Hong Kong', 'India', 'Indonesia', 'Iran',
+		      'Iraq', 'Japan', 'Jordan', 'Kuwait', 'Kyrgyz Republic', 'Laos',
+		      'Lebanon', 'Macao', 'Malaysia', 'Mongolia', 'Myanmar', 'Nepal',
+		      'Oman', 'Pakistan', 'Philippines', 'Qatar', 'Saudi Arabia',
+		      'Singapore', 'South Korea', 'Sri Lanka', 'Palestine', 'Syria',
+		      'Taiwan', 'Tajikistan', 'Thailand', 'Timor-Leste', 'Turkey', 
+		      'Turkmenistan', 'United Arab Emirates', 'Uzbekistan', 'Vietnam',
+		      'Yemen')
 ORDER BY 
 countryname ASC,
 datec ASC;
