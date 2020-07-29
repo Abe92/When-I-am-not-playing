c6b6971ddcc1b79e8d8ae264605e5d7cef684181
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-07-29 10:59:01 +0100

    Update AfricanQuery.sql
    - Fix typo ('Bostwana' to 'Botswana')
    - Add new entry ('Zimbabwe')

--- Programming/SQL Scripts/AfricanQuery.sql
+++ Programming/SQL Scripts/AfricanQuery.sql
@@ -3,16 +3,16 @@ SQL Query to call all available African countries from 'govt_respond_tracker' ta
 */
 
 SELECT * FROM govt_respond_tracker
-WHERE countryname IN ('Algeria', 'Angola', 'Benin', 'Bostwana',
+WHERE countryname IN ('Algeria', 'Angola', 'Benin', 'Botswana',
 		      'Burkina Faso', 'Burundi', 'Cameroon', 'Cape Verde',
-	              'Central African Republic', 'Chad', 'Congo', 'Cote d''Ivoire',
-		      'Democratic Republic of the Congo', 'Djibouti', 'Egypt',
+		      'Central African Republic', 'Chad', 'Congo', 'Cote d''Ivoire',
+		      'Democratic Republic of Congo', 'Djibouti', 'Egypt',
 		      'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia',
 		      'Ghana', 'Guinea', 'Kenya', 'Lesotho', 'Liberia', 'Libya',
-	              'Madagascar', 'Malawi', 'Mauritania', 'Mauritius', 'Morocco',
+		      'Madagascar', 'Mali', 'Malawi', 'Mauritania', 'Mauritius', 'Morocco',
 		      'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda', 'Seychelles',
-		      'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan',
-		      'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia')
+		      'Sierra Leone', 'Senegal','Somalia', 'South Africa', 'South Sudan', 'Sudan',
+		      'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe')
 ORDER BY 
 countryname ASC,
 datec ASC;
