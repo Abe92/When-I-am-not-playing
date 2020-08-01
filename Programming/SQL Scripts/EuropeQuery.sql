a439f6eed0dd95448d7bf03ec07f58b461276537
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-08-01 09:04:51 +0100

    Update EuropeQuery.sql
    Fixing indentations

--- Programming/SQL Scripts/EuropeQuery.sql
+++ Programming/SQL Scripts/EuropeQuery.sql
@@ -4,13 +4,13 @@ SQL Query to call all available European countries from 'govt_respond_tracker' t
 
 SELECT * FROM govt_respond_tracker
 WHERE countryname IN ('Albania', 'Andorra', 'Austria', 'Belarus', 'Belgium',
-					 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Czech Republic',
-					 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece',
-					 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Kosovo', 'Latvia',
-					 'Lithuania', 'Luxembourg', 'Moldova', 'Netherlands', 'Norway',
-					 'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino', 'Serbia',
-					 'Slovak Republic', 'Slovenia', 'Spain', 'Sweden', 'Switzerland',
-					 'Ukraine', 'United Kingdom')
+		      'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Czech Republic',
+		      'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece',
+		      'Hungary', 'Iceland', 'Ireland', 'Italy', 'Kosovo', 'Latvia',
+		      'Lithuania', 'Luxembourg', 'Moldova', 'Netherlands', 'Norway',
+		      'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino', 'Serbia',
+		      'Slovak Republic', 'Slovenia', 'Spain', 'Sweden', 'Switzerland',
+		      'Ukraine', 'United Kingdom')
 ORDER BY 
 countryname ASC,
 datec ASC;
