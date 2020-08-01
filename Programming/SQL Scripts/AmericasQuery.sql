c0578b726b7c5c0d9226982cd740d84fcfeaa08e
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-08-01 08:50:19 +0100

    Update AmericasQuery.sql
    Updating the countries list; adding 'Greenland'

--- Programming/SQL Scripts/AmericasQuery.sql
+++ Programming/SQL Scripts/AmericasQuery.sql
@@ -6,7 +6,7 @@ SELECT * FROM govt_respond_tracker
 WHERE countryname IN ('Argentina', 'Barbados', 'Belize', 'Bolivia', 'Brazil',
 		      'Canada', 'Chile', 'Colombia', 'Costa Rica', 'Cuba', 'Dominica',
 		      'Dominican Republic', 'Ecuador', 'El Salvador', 'Guatemala', 'Guyana',
-		      'Haiti', 'Honduras', 'Jamaica', 'Mexico', 'Nicaragua', 'Panama',
+		      'Greenland', 'Haiti', 'Honduras', 'Jamaica', 'Mexico', 'Nicaragua', 'Panama',
 		      'Paraguay', 'Peru', 'Suriname', 'Trinidad and Tobago', 'United States',
 		      'Uruguay', 'Venezuela')
 ORDER BY 
