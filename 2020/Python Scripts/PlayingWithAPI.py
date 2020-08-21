8242cb7760aa7a59c779dce7a274fd1b18e80a7a
Aldy Rasyid Abe <Abe92@users.noreply.github.com>
2020-08-21 06:19:37 +0100

    Update PlayingWithAPI.py
    Specifying the limit of entries to fetch from the API endpoints, denoted by '?limit=1000'
    
    Credits to Nobel Media Team.

--- 2020/Python Scripts/PlayingWithAPI.py
+++ 2020/Python Scripts/PlayingWithAPI.py
@@ -14,12 +14,13 @@ import json
 ''' 
 API Endpoint
 '''
-url = 'https://api.nobelprize.org/2.0/laureates'
+url_1 = 'https://api.nobelprize.org/2.0/laureates?limit=1000'                # '?limit=1000', determine the max amount of entries, laureates has at least 950 entries
+url_2 = 'https://api.nobelprize.org/2.0/nobelPrizes?limit=1000'              # '?limit=1000', determine the max amount of entries, nobel prizes has at least 650 entries
 
 '''
 Requesting API server status code
 '''
-response = requests.get(url)
+response = requests.get('url_1 or url_2, pick one')
 print(response.status_code)
 
 '''
@@ -31,5 +32,5 @@ print(json.dumps(laureates_data, sort_keys=True, indent=4))
 '''
 Writing JSON to a file with Pretty-Printing
 '''
-with open('Nobel_Laureates.json', 'w') as outfile:
+with open('file-name.json', 'w') as outfile:
     json.dump(laureates_data, outfile, sort_keys=True, indent=4)
